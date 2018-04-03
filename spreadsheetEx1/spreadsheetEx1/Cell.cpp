9513ba071269b732857ce950c0434b8f4e7d08c2
s2087650 <daniellinye@hotmail.com>
2018-04-03 17:27:46 +0200

    added starting files, ex 2 done, TODO ex 3

--- spreadsheetEx1/spreadsheetEx1/Cell.cpp
+++ spreadsheetEx1/spreadsheetEx1/Cell.cpp
@@ -1,6 +1,115 @@
 #include "stdafx.h"
+#include <memory>
+#include <sstream>
 
-struct cell
+class CellValueBase
 {
+private:
+	int iv; std::string sv; float fv;
+public:
+	virtual void destroy();
+	virtual std::stringstream print();
+	virtual float convtn();
 
+};
+
+template<typename T>
+class CellValue final : public CellValueBase
+{
+private:
+	T tvalue;
+public:
+	virtual void destroy();
+	virtual std::stringstream print();
+	virtual float convtn();
+	CellValue(T init)
+	{
+		this->tvalue = init;
+	}
+
+	T formvalue()
+	{
+		return tvalue;
+	}
+};
+
+void CellValue<int>::destroy()
+{
+	delete this;
+}
+
+void CellValue<float>::destroy()
+{
+	delete this;
+}
+
+void CellValue<std::string>::destroy()
+{
+	delete this;
+}
+
+std::stringstream CellValue<int>::print()
+{
+	std::stringstream ss;
+	ss << tvalue;
+	return ss;
+}
+
+std::stringstream CellValue<float>::print()
+{
+	std::stringstream ss;
+	ss << tvalue;
+	return ss;
+}
+
+std::stringstream CellValue<std::string>::print()
+{
+	std::stringstream ss;
+	ss << tvalue;
+	return ss;
+}
+
+float CellValue<int>::convtn()
+{
+	float temp = (float)tvalue;
+	return temp;
+}
+
+float CellValue<float>::convtn()
+{
+	return tvalue;
+}
+
+float CellValue<std::string>::convtn()
+{
+	int temp = 0;
+	for (char chars : tvalue)
+	{
+		if(chars >= '0' && chars <= '9')
+			temp = temp * 10 + (chars - '0');
+	}
+	return temp;
+}
+
+class Cell
+{
+private:
+	std::unique_ptr<CellValueBase> *value = nullptr;
+public:
+	template<typename T>
+	void initCell()
+	{
+		value = new CellValue<T>;
+	}
+
+	void initCell(CellValue<float>* val)
+	{
+
+	}
+
+	void initCell(CellValue<std::string>* val)
+	{
+
+	}
+	
 };
--- spreadsheetEx1/spreadsheetEx1/Sheet.cpp
+++ spreadsheetEx1/spreadsheetEx1/Sheet.cpp
@@ -1,37 +1,21 @@
 #include "stdafx.h"
 #include "Cell.cpp"
-#include "Sheet.o"
+#include <vector>
+#include <memory>
+#include <sstream>
 
 
-class sheet
+class Sheet
 {
 private:
-	cell **spreadsheet;
+	std::vector<std::vector<Cell>> cells;
 	int h, b;
 public:
-	void initsheet(int h, int b)
+	Sheet(int h, int b)
 	{
-		cell **spsheet = new cell*[h];
-		for (int i = 0; i < h; i++)
-		{
-			spsheet[i] = new cell[b];
-		}
-		this->spreadsheet = spsheet;
-		this->h = h; 
+		this->h = h;
 		this->b = b;
 	}
 
-	void resizesheet(int h, int b)
-	{
-		cell **spsheet = this->spreadsheet;
-		int temph = this->h, tempb = this->b;
-		initsheet(h, b);
-		for (int i = 0; i < h && i < temph; i++)
-		{
-			for (int j = 0; j < b && j < tempb; j++)
-			{
-				this->spreadsheet[i][j] = spsheet[i][j];
-			}
-		}
-	}
+
 };
