23f2d15f793b4ad341cabb0480274ffea79ab68d
Daniël <daniellinye@hotmail.com>
2018-04-05 16:43:56 +0200

    removed completely stupid function

--- opdracht2/CellAddress.cpp
+++ opdracht2/CellAddress.cpp
@@ -15,18 +15,6 @@ public:
 		this->y = y;
 	}
 
-	void init(char input[])
-	{
-		if (input[0] - 'A' >= 0)
-			this->x = input[0] - 'A';
-		for (int i = 1; i < sizeof(input) - 1; i++)
-			if (input[i] - '0' >= 0 && input[i] - '0' <= 9)
-				this->y = this->y * 10 + input[i] - '0';
-			else
-				std::cout << "Value was not defined!" << "\n";
-	}
-
-	//actually createfromreference, but for convenience sake
 	void init(std::string input)
 	{
 		if (input[0] - 'A' >= 0)
