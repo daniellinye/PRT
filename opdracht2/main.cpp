e36b20091b49b0dda916671c6ecf63e2b7a0c510
s2087650 <daniellinye@hotmail.com>
2018-04-14 12:21:17 +0200

    hm

--- opdracht2/main.cpp
+++ opdracht2/main.cpp
@@ -1,12 +1,9 @@
 // spreadsheetEx1.cpp : Defines the entry point for the console application.
 //
 
-#include "SheetView.cpp"
+#include "Range.cpp"
 
 int main()
 {
-	SheetView view = SheetView();
-
-	view.Display();
   return 0;
 }
