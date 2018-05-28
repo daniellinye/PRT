ad786b55a13ef540290a41e4547c364a36dbaac0
Daniël <daniellinye@hotmail.com>
2018-05-28 15:15:24 +0200

    distributed classes into different files

---
+++ opdracht3/visualstudioserver/ChatServer/ChatServer/ConnectionFunctions.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using ChatServer;
+
+
+namespace ChatServer
+{
+
+    //is a stuct such that we can give users variables whilst online
+    public class TcpUsers : TcpClient
+    {
+        TcpClient client;
+        String name;
+        int id;
+
+        public TcpUsers(TcpClient listener, String newname, int nid)
+        {
+            name = newname;
+            client = listener;
+            id = nid;
+        }
+
+        public String ReturnName()
+        {
+            return name;
+        }
+
+        public int ReturnId()
+        {
+            return id;
+        }
+    }
+
+    public class ConnectionFunctions
+    {
+        public List<TcpUsers> clients = new List<TcpUsers>();
+
+        public ConnectionFunctions() { }
+
+        //adds new client if the user is not logged in yet
+        public void LoginUser(TcpClient client, String name)
+        {
+            foreach (TcpClient c in clients)
+            {
+                if (c.Equals(client))
+                {
+                    Console.WriteLine("User " + name + " is already logged in");
+                    return;
+                }
+            }
+            Console.WriteLine("User: " + name + " Logged in");
+            clients.Add(new TcpUsers(client, name, clients.Count));
+        }
+
+        public void LogoutUser(String name, int id)
+        {
+            foreach (TcpUsers c in clients)
+            {
+                if (c.ReturnName() == name && c.ReturnId() == id)
+                {
+                    c.Close();
+                    clients.Remove(c);
+                    return;
+                }
+            }
+            Console.WriteLine("Error 404:Element was not found in list");
+        }
+
+        //pings other clients depended on the messagesender
+        public void Ping(String message, int id)
+        {
+            //TODO: implement
+        }
+
+        public TcpUsers GetMostRecent()
+        {
+            return clients[clients.Count - 1];
+        }
+
+        public TcpUsers GetName(int id)
+        {
+            return clients[id];
+        }
+
+    }
+
+    class ParseFunctions
+    {
+        ConnectionFunctions cf;
+        DatabaseFunctions df;
+        public ParseFunctions()
+        {
+            cf = new ConnectionFunctions();
+            df = new DatabaseFunctions();
+        }
+
+        public bool Parser(String input, TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            //TODO: REMINDER
+            //whenever a user is logged in, add the current connection to the clients of ConnectionFunctions
+
+            String[] command = input.Split(':');
+
+            switch (command[0])
+            {
+                case "Ping":
+                    StreamWrite("Pong", stream);
+                    break;
+                case "Login":
+                    string[] lparser = command[1].Split('.');
+                    string username = lparser[0];
+                    string password = lparser[1];
+                    return Login(username, password, client);
+
+                //standardthing
+                case "Message":
+                    string[] uparser = command[1].Split(',');
+                    string[] mparser = uparser[1].Split('.');
+                    string user = uparser[0];
+                    string recipient = mparser[0];
+                    string message = mparser[1];
+                    Message(user, recipient, message, client);
+                    break;
+                default:
+                    Console.WriteLine("Command " + command[0] + " was not implemented");
+                    StreamWrite("Error:001, command not found", stream);
+                    break;
+            }
+            return false;
+        }
+
+        //reads rawdata from the newworkstream :D
+        public String Read(NetworkStream stream)
+        {
+            byte[] myReadBuffer = new byte[1024];
+            String responseData = String.Empty;
+            Int32 bytes = stream.Read(myReadBuffer, 0, myReadBuffer.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(myReadBuffer, 0, bytes);
+            return responseData;
+        }
+
+        //writes rawdata to the newtworkstream :D
+        public String StreamWrite(String input, NetworkStream stream)
+        {
+            Byte[] login = System.Text.Encoding.ASCII.GetBytes(input);
+            stream.Write(login, 0, login.Length);
+            Console.WriteLine(input);
+            return input;
+        }
+
+        //format has to be: "username.password"
+        public bool Login(string username, string password, TcpClient client)
+        {
+            //TODO: throw check to database
+            //that returns null if not found
+            if (df.ExecuteFunction("Login", username + "." + password) != null)
+            {
+                cf.LoginUser(client, username);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Logout(string username, string password, TcpClient client)
+        {
+
+            return false;
+        }
+
+        public void Message(string username, string password, string message, TcpClient client)
+        {
+            //TODO
+            //PING TO OTHER CLIENTS
+
+            StreamWrite("Method is not implemented yet", client.GetStream());
+            //PING OTHER USERS
+
+            //TODO
+            //ADD TO DATABASE
+        }
+
+        public string LoadMessages(string username, string password, string otheruser)
+        {
+            //TODO
+            //PING TO DATABASE TO RETRIEVE
+            //MESSAGES
+
+            return null;
+        }
+
+
+    }
+
+}
---
+++ opdracht3/visualstudioserver/ChatServer/ChatServer/Database.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+
+namespace ChatServer
+{
+    class Database
+    {
+
+    }
+
+
+    class DatabaseFunctions
+    {
+        public DatabaseFunctions()
+        {
+
+        }
+
+        //executes a function on the database and returns values
+        public string ExecuteFunction(string function, string args)
+        {
+            //TODO:
+            //add basic functionalities to login, getmessages
+            //sendmessages, getuser, sendmessages and login
+            switch (function)
+            {
+                default:
+                    Console.WriteLine("DatabaseFunction: " + function + " not recognized or implemented.");
+                    break;
+            }
+
+            return null;
+        }
+
+        public void LogIn(string username, string password)
+        {
+
+            try
+            {
+                //Is now hardcoded but should be an online service
+                //service is not available yet, so this is hardcoded
+                //for testing: change the Path after AttachDbFilename=
+                using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Studie\\PRT\\opdracht3\\visualstudioserver\\ChatServer\\ChatServer\\users.mdf;Integrated Security=True"))
+                {
+                    Console.WriteLine("Opening connection");
+                    connection.Open();
+                    Executecommand(connection, "DELETE FROM users");
+                    Executecommand(connection, "INSERT INTO users (name,password) VALUES ('" + username + "','" + password + "')");
+
+                    SqlDataReader reader = GetTables(connection, "SELECT * FROM users");
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["name"].ToString());
+                    }
+                }
+                Console.WriteLine("Connection Successful");
+            }
+            catch
+            {
+                Console.WriteLine("Connection Unsuccesfull");
+            }
+
+        }
+
+        public void Executecommand(SqlConnection dbconnection, string query)
+        {
+            try
+            {
+                SqlCommand sqlcommand = new SqlCommand(query, dbconnection);
+                sqlcommand.ExecuteNonQuery();
+            }
+            catch
+            {
+                Console.WriteLine(query);
+            }
+        }
+
+
+        public SqlDataReader GetTables(SqlConnection dbconnection, string query)
+        {
+            SqlCommand sqlcommand = new SqlCommand();
+            sqlcommand.CommandText = query;
+            sqlcommand.Connection = dbconnection;
+
+            return sqlcommand.ExecuteReader();
+        }
+    }
+
+}
