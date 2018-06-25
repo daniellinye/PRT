5f241cce5f318bd26286f70a4d1b35182afcd45b
s2087650 <daniellinye@hotmail.com>
2018-06-25 14:26:11 +0200

    added new testclass and cleanup

--- opdracht2/main.cpp
+++ opdracht2/main.cpp
@@ -2,11 +2,42 @@
 
 #include "SheetView.h"
 #include <iostream>
+#include <assert.h>
 
 using namespace std;
 
+
+class MainTests
+{
+private:
+	SheetView sv;
+public:
+	//constructor
+	MainTests()
+	{
+		sv = SheetView();
+	}
+	//*******************************************
+	//tests
+
+	void dotests()
+	{
+		assert(test1());
+	}
+
+	bool test1()
+	{
+		return true;
+	}
+};
+
 int main()
 {
+	//initialize and to unit assert tests
+	MainTests mt = MainTests();
+	mt.dotests();
+
+	//execute actual program
 	SheetView view = SheetView();
 	view.Display();
 	return 0;
